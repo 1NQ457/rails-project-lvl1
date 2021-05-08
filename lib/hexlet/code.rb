949c9a93d77d1f34e16c87d1d275742c45cc2341
1NQ457 <alex.kot14@gmail.com>
2021-05-08 21:30:10 +0300

    project init

---
+++ lib/hexlet/code.rb
@@ -0,0 +1,8 @@
+require "hexlet/code/version"
+
+module Hexlet
+  module Code
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/hexlet/code/version.rb
@@ -0,0 +1,5 @@
+module Hexlet
+  module Code
+    VERSION = "0.1.0"
+  end
+end
