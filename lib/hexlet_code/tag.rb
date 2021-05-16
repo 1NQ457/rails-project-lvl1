b74b7c434f4941f799b6ebd632a8bc9420132c43
1NQ457 <alex.kot14@gmail.com>
2021-05-16 14:32:09 +0300

    code formatting

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 module HexletCode
+  # Tag Builder
   class Tag
     def self.build(name, **attributes)
       body = yield if block_given?
 
-      formatted_attributes = attributes.each_with_object([]) do |(name, content), acc|
-        acc.push("#{name}=\"#{content}\"")
+      formatted_attributes = attributes.each_with_object([]) do |(attribute, content), acc|
+        acc.push("#{attribute}=\"#{content}\"")
       end
 
       tag = [name, *formatted_attributes].join(' ')
