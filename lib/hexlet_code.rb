1b93453c8fb343fc39d179c6f2e88698a6cb078f
1NQ457 <alex.kot14@gmail.com>
2021-05-16 19:40:39 +0300

    form_for added

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,5 +5,12 @@ require 'hexlet_code/tag'
 
 module HexletCode
   class Error < StandardError; end
+
+  class << self
+    def form_for(subject, route = '#')
+      body = yield if block_given?
+      HexletCode::Tag.build('form', action: route, mathod: 'Post') {body ? body : ''}
+    end
+  end
   # Your code goes here...
 end
